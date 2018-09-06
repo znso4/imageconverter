d3602eacb181084828fe9f8f88343b9578022ca1
znso4 <znso4@qq.com>
2018-09-06 09:36:14 +0800

    backup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,12 +3,56 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow),
+    filenamesModel(new QStringListModel(this)){
     ui->setupUi(this);
+    ui->lvFiles->setModel(filenamesModel);
+    ui->lvFiles->setUpdatesEnabled(true);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::selectFiles(){
+    addFile(QFileDialog::getOpenFileNames(
+                this,tr("选择"), ".", tr("Picture (*.jpg;*.bmp;*.png)")));
+}
+
+void MainWindow::addFile(QStringList lst){
+    QStringList nlst;
+    for(auto iter : lst){
+        if(filePaths.contains(iter)){continue;}
+        nlst += iter.section("/", -1, -1).section("\\", -1, -1);
+    }
+    filePaths += lst;
+    filenamesModel->setStringList(filenamesModel->stringList() + nlst);
+    qDebug()<<filePaths<<endl;
+}
+
+void MainWindow::removeFiles(){
+    auto idxs = ui->lvFiles->selectionModel()->selectedIndexes();
+    for(auto idx : idxs){
+        int i = idx.row();
+        filePaths.removeAt(i);
+        filenamesModel->removeRow(i);
+    }
+    qDebug()<<filePaths<<endl;
+}
+
+void MainWindow::dragEnterEvent(QDragEnterEvent *event){
+    auto url = event->mimeData()->data(QString("text/uri-list"));
+    qDebug()<<url<<endl;
+    if(event->source() != this && event->mimeData()->hasUrls()){
+//        url.startsWith("file:///");
+//        url.endsWith(".jpg");
+    }else{
+        event->ignore();
+    }
+}
+
+void MainWindow::dropEvent(QDropEvent *event){
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QFileDialog>
+#include <QDragEnterEvent>
+#include <QDropEvent>
+#include <QMimeData>
+#include <QDebug>
+#include <QStringListModel>
 #include <QMainWindow>
 
 namespace Ui {
@@ -12,11 +17,23 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
 
+public slots:
+    void selectFiles();
+    void removeFiles();
+protected:
+    void dragEnterEvent(QDragEnterEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
 private:
     Ui::MainWindow *ui;
+    QStringListModel *filenamesModel;
+    QStringList filePaths;
+
+    void addFile(QStringList);
+
+
 };
 
 #endif // MAINWINDOW_H
